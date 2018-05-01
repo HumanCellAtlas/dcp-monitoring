c3b7db9e50c6bcd660bf3f421fa5cd8e627a5c79
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-04-30 23:43:34 -0700

    DCP health is comprised of DSS && Ingest && Analysis

--- dcp.tf
+++ dcp.tf
@@ -1,9 +1,11 @@
 // DCP overall system health
 resource "aws_route53_health_check" "dcp" {
   type                   = "CALCULATED"
-  child_health_threshold = 1
+  child_health_threshold = 3
   child_healthchecks     = [
-    "${aws_route53_health_check.dss.id}"
+    "${aws_route53_health_check.dss.id}",
+    "${aws_route53_health_check.ingest.id}",
+    "${aws_route53_health_check.analysis.id}"
   ]
 
   tags = {
