b6e0734fda217f9eddeff6a42e8580a17e2fdc4e
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-05-30 14:47:22 -0700

    Increase interval between health checks

--- dss.tf
+++ dss.tf
@@ -4,7 +4,7 @@ resource "aws_route53_health_check" "dss" {
   type              = "HTTPS"
   resource_path     = "/internal/application_secrets"
   failure_threshold = "3"
-  request_interval  = "30"
+  request_interval  = "60"
   cloudwatch_alarm_region = "${var.aws_region}"
 
   tags = {
