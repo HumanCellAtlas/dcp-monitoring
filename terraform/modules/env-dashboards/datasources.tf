21b4dd7cafa72568d62076eece368a618325b33c
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-09-24 23:09:34 -0700

    Add templated data sources and dashboards for upload service (#6)
    * WIP
    
    * upload db wip
    
    * updated upload dasboard template
    
    * Update env-dashboards README.md
    
    * Squash me later
    
    * updates with jq and other changes for upload dashboard
    
    * upload dashboards fixed

--- terraform/modules/env-dashboards/datasources.tf
+++ terraform/modules/env-dashboards/datasources.tf
@@ -1,6 +1,7 @@
 locals {
   gcp_analysis_datasource_name = "gcp-analysis-${var.env}"
   aws_upload                   = "aws-upload"
+  aws_upload_datasource_name    = "upload-db-${var.env}"
 
   gcp_analysis_datasource = <<EOF
 {
@@ -16,4 +17,40 @@ locals {
   "readOnly": false
 }
 EOF
+
+  aws_upload_datasource = <<EOF
+{
+  "name": "${local.aws_upload_datasource_name}",
+  "type": "postgres",
+  "url": "${data.external.upload_db_secrets_processing.result.host}",
+  "user": "${data.external.upload_db_secrets_processing.result.username}",
+  "database": "upload_${var.env}",
+  "access": "proxy",
+  "secureJsonData": {
+    "password": "${data.external.upload_db_secrets_processing.result.password}"
+  },
+  "jsonData": {
+    "access": "proxy",
+    "sslmode": "disable"
+  },
+  "readOnly": false
+}
+EOF
+}
+
+data "aws_secretsmanager_secret" "upload_db_creds" {
+  name = "dcp/upload/${var.env}/database"
+}
+
+data "aws_secretsmanager_secret_version" "upload_db_creds" {
+  secret_id = "${data.aws_secretsmanager_secret.upload_db_creds.id}"
+}
+
+data "external" "upload_db_secrets_processing" {
+  program = ["sh", "${path.root}/../../../../terraform/modules/env-dashboards/process_upload_db_secrets.sh"]
+   query = {
+    # arbitrary map from strings to strings, passed
+    # to the external program as the data query.
+    db_secret_string = "${data.aws_secretsmanager_secret_version.upload_db_creds.secret_string}"
+ }
 }
--- terraform/modules/env-dashboards/outputs.tf
+++ terraform/modules/env-dashboards/outputs.tf
@@ -2,10 +2,15 @@ output "gcp_analysis_datasource_name" {
   value = "${local.gcp_analysis_datasource_name}"
 }
 
+output "aws_upload_datasource_name" {
+  value = "${local.aws_upload_datasource_name}"
+}
+
 output "datasources" {
   value = <<EOF
 [
-  ${local.gcp_analysis_datasource}
+  ${local.gcp_analysis_datasource},
+  ${local.aws_upload_datasource}
 ]
 EOF
 }
@@ -14,7 +19,8 @@ output "dashboards" {
   value = <<EOF
 [
   ${local.dcp_dashboard},
-  ${local.dss_dashboard}
+  ${local.dss_dashboard},
+  ${local.upload_dashboard}
 ]
 EOF
 }
