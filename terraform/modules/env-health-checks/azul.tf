fb8ffe2e44c8d41db82ecc7cbbe2ccc87feab9ae
Matt Weiden <538456+mweiden@users.noreply.github.com>
2019-02-27 14:50:06 -0800

    Fix formatting and docs

--- terraform/modules/env-health-checks/azul.tf
+++ terraform/modules/env-health-checks/azul.tf
@@ -1,5 +1,5 @@
 data "external" "azul_health_check_id" {
-  program = [ "sh", "../../../../scripts/get_health_check_id" ]
+  program = ["sh", "../../../../scripts/get_health_check_id"]
 
   query = {
     caller_reference_prefix = "azul-composite-${var.env}"
