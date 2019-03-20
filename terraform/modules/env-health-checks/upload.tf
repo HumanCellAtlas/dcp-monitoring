7f66875d14ef473af3d2792d94673d784febb0ef
parthshahva <parthshahva@gmail.com>
2019-03-20 12:32:07 -0700

    use /health instead of /v1/health for upload health check endpoint

--- terraform/modules/env-health-checks/upload.tf
+++ terraform/modules/env-health-checks/upload.tf
@@ -2,7 +2,7 @@ resource "aws_route53_health_check" "upload" {
   fqdn                    = "upload.${(var.env == "prod") ? "" : "${var.env}."}data.humancellatlas.org"
   port                    = 443
   type                    = "HTTPS"
-  resource_path           = "/v1/health"
+  resource_path           = "/health"
   failure_threshold       = "3"
   request_interval        = "30"
   cloudwatch_alarm_region = "${var.region}"
